f640f78cc84e6b639e67aeafd2f597be24f982d8
Benjamin Semah <benjaminsemah@gmail.com>
2021-12-16 09:28:59 +0000

    Check linters errors

--- projects.js
+++ projects.js
@@ -1,58 +1,57 @@
-
-let projectObjects = [
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+const projectObjects = [
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    } 
-]
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+];
 
-const createListItem = (technology)=>{
-    let listItem = document.createElement('li');
-    listItem.textContent = technology;
-    return listItem;
-}
-const makeTechnologies = (technologies=[],index)=>{
-    
-    let project = document.querySelector(`#project${index+1} ul`)
-     technologies.map((technology)=>createListItem(technology)).forEach((node)=>{
-         project.appendChild(node)
-     })   
-}
+const createListItem = (technology) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = technology;
+  return listItem;
+};
+const makeTechnologies = (technologies = [], index) => {
+  const project = document.querySelector(`#project${index + 1} ul`);
+  technologies.map((technology) => createListItem(technology)).forEach((node) => {
+    project.appendChild(node);
+  });
+};
 
-const makeProject = ({name,description,featuredImage,technologies},index)=>{
-
-    let projectDiv = document.createElement('div')
-    let projectId = `project${index+1}`;
-    projectDiv.setAttribute('id', projectId);
-    projectDiv.innerHTML = `
+const makeProject = ({
+  name, description, featuredImage,
+}, index) => {
+  const projectDiv = document.createElement('div');
+  const projectId = `project${index + 1}`;
+  projectDiv.setAttribute('id', projectId);
+  projectDiv.innerHTML = `
     <a class="projectImages" href="#">
     <img
         class="imageNumber1"
@@ -73,32 +72,28 @@ const makeProject = ({name,description,featuredImage,technologies},index)=>{
         <button class="projectButton" type="Button">See Project</button>
     </div>
     `;
-    let myProjects = document.querySelector('.myProjects')
-    myProjects.appendChild(projectDiv)
-    return projectId
-}
+  const myProjects = document.querySelector('.myProjects');
+  myProjects.appendChild(projectDiv);
+  return projectId;
+};
 
-projectObjects.forEach((object,index)=>makeProject(object,index))
-projectObjects.forEach((object,index)=>makeTechnologies(object.technologies,index))
+projectObjects.forEach((object, index) => makeProject(object, index));
+projectObjects.forEach((object, index) => makeTechnologies(object.technologies, index));
 
-const projectModal = document.querySelector('.popupObject')
-const projectWrap =document.querySelector('.popupWrapper')
-const seeProjectBtn = Array.from(document.querySelectorAll('.projectButton'))
-Object.keys(seeProjectBtn).forEach((k) =>{
-    seeProjectBtn[k].addEventListener('click', () => {
-        console.log('Test Work')
-        projectWrap.classList.toggle('hideModal')
-        projectModal.classList.toggle('hideModal')
-           })
+const projectModal = document.querySelector('.popupObject');
+const projectWrap = document.querySelector('.popupWrapper');
+const seeProjectBtn = Array.from(document.querySelectorAll('.projectButton'));
+Object.keys(seeProjectBtn).forEach((k) => {
+  seeProjectBtn[k].addEventListener('click', () => {
+    projectWrap.classList.toggle('hideModal');
+    projectModal.classList.toggle('hideModal');
+  });
 });
 
 const cancelBtn = Array.from(document.querySelectorAll('.popupClose'));
-Object.keys(cancelBtn).forEach(k =>{
-cancelBtn[k].addEventListener('click',()=>{
-    projectWrap.classList.toggle('hideModal')
-    projectModal.classList.toggle('hideModal')
-})
-})
-
-
-
+Object.keys(cancelBtn).forEach((k) => {
+  cancelBtn[k].addEventListener('click', () => {
+    projectWrap.classList.toggle('hideModal');
+    projectModal.classList.toggle('hideModal');
+  });
+});
