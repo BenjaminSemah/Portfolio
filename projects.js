f8d9b039e4487e81156e296b85b20351b8053b4e
Benjamin Semah <benjaminsemah@gmail.com>
2021-12-16 18:21:37 +0000

    Add form validation

--- projects.js
+++ projects.js
@@ -1,58 +1,57 @@
-
-let projectObjects = [
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+const projectObjects = [
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    },
-    {
-        name: 'Multi-Post Stories',
-        description : `A daily selection of privately personalized reads; no accounts or
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
         sign-ups required. has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a standard dummy text.`,
-        featuredImage: './assets/Img-Placeholder.svg',
-        technologies: ['css','html','bootstrap','ruby']
-    } 
-]
+    featuredImage: './assets/Img-Placeholder.svg',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  },
+];
 
-const createListItem = (technology)=>{
-    let listItem = document.createElement('li');
-    listItem.textContent = technology;
-    return listItem;
-}
-const makeTechnologies = (technologies=[],index)=>{
-    
-    let project = document.querySelector(`#project${index+1} ul`)
-     technologies.map((technology)=>createListItem(technology)).forEach((node)=>{
-         project.appendChild(node)
-     })   
-}
+const createListItem = (technology) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = technology;
+  return listItem;
+};
+const makeTechnologies = (technologies = [], index) => {
+  const project = document.querySelector(`#project${index + 1} ul`);
+  technologies.map((technology) => createListItem(technology)).forEach((node) => {
+    project.appendChild(node);
+  });
+};
 
-const makeProject = ({name,description,featuredImage,technologies},index)=>{
-
-    let projectDiv = document.createElement('div')
-    let projectId = `project${index+1}`;
-    projectDiv.setAttribute('id', projectId);
-    projectDiv.innerHTML = `
+const makeProject = ({
+  name, description, featuredImage,
+}, index) => {
+  const projectDiv = document.createElement('div');
+  const projectId = `project${index + 1}`;
+  projectDiv.setAttribute('id', projectId);
+  projectDiv.innerHTML = `
     <a class="projectImages" href="#">
     <img
         class="imageNumber1"
@@ -73,20 +72,20 @@ const makeProject = ({name,description,featuredImage,technologies},index)=>{
         <button class="projectButton" type="Button">See Project</button>
     </div>
     `;
-    let myProjects = document.querySelector('.myProjects')
-    myProjects.appendChild(projectDiv)
-    return projectId
-}
+  const myProjects = document.querySelector('.myProjects');
+  myProjects.appendChild(projectDiv);
+  return projectId;
+};
 
-projectObjects.forEach((object,index)=>makeProject(object,index))
-projectObjects.forEach((object,index)=>makeTechnologies(object.technologies,index))
+projectObjects.forEach((object, index) => makeProject(object, index));
+projectObjects.forEach((object, index) => makeTechnologies(object.technologies, index));
 
-document.querySelectorAll('.myProjects .projectButton').forEach((projectBtn) =>{
-    projectBtn.addEventListener('click', () => {
-        document.querySelector('.popupObject').style.display='block';
-    })
+document.querySelectorAll('.myProjects .projectButton').forEach((projectBtn) => {
+  projectBtn.addEventListener('click', () => {
+    document.querySelector('.popupObject').style.display = 'block';
+  });
 });
 
 document.querySelector('.popupClose').addEventListener('click', () => {
-    document.querySelector('.popupObject').style.display='none';
+  document.querySelector('.popupObject').style.display = 'none';
 });
--- validation.js
+++ validation.js
@@ -1,44 +1,17 @@
-const error = document.querySelector('.error')
-const email = document.querySelector('#email')
+const error = document.querySelector('.error');
+const email = document.querySelector('#email');
 const submit = document.querySelector('#submit');
-const check = (node)=>console.log(node)
 
-const checkLowerCase = ()=>{
-    if(email.validity.patternMismatch) error.textContent = 'Email should be lowercase' 
-        
-    
-    else   error.textContent = ''
-    return !email.validity.patternMismatch;
-    
-}
+const checkLowerCase = () => {
+  if (email.validity.patternMismatch) error.textContent = 'The form has not been submitted. Please type email iin lowercase';
+  else error.textContent = '';
+  return !email.validity.patternMismatch;
+};
 
-submit.addEventListener('submit',()=>{
-    checkLowerCase()
-})
+submit.addEventListener('submit', () => {
+  checkLowerCase();
+});
 
-email.addEventListener('input',( )=>{
-    checkLowerCase()
-})
-
-// const email = document.querySelector('#email');
-// const submit = document.querySelector('#submit');
-// let error = document.querySelector('.error');
-
-// function validateEmail (event) {
-//   if (email.validity.typeMismatch){
-//     error.textContent = 'Please input a valid email.';
-//     event.preventDefault()
-//   } else {
-//     error.textContent = '';
-//   }
-// };
-
-// email.addEventListener('input', (event) => {
-//   validateEmail(event);
-//   con;sole.log('aaaa')
-// })
-
-// submit.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   validateEmail(event);
-// });
+email.addEventListener('input', () => {
+  checkLowerCase();
+});
