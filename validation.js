3c723fae6b6d9342389286a99e1951063eedacc0
Benjamin Semah <benjaminsemah@gmail.com>
2021-12-16 21:52:18 +0000

    Update form validation

--- validation.js
+++ validation.js
@@ -1,29 +1,27 @@
-const error = document.querySelector('.error');
+let error = document.querySelector('.error');
 const email = document.getElementById('email');
-const submit = document.getElementById('submit');
 const myForm = document.getElementById('myform');
 let testBol = false;
 let emailChecker = '';
 
-//ddddddddddddddddddddd
+// ddddddddddddddddddddd
 email.addEventListener('keyup', () => {
   emailChecker = email.value.toLowerCase();
 });
 
 myForm.addEventListener('submit', (e) => {
   if (emailChecker !== email.value) {
-    error.innerText = 'The form has not been submitted. Please type email iin lowercase';
+    error.innerText = 'Form not submitted. Please type email in lowercase';
     if (testBol === false) {
       error.classList.toggle('hideError');
     }
     testBol = true;
-  
+
     email.addEventListener('click', () => {
       if (testBol === true) {
         error.classList.toggle('hideError');
         testBol = false;
       }
-      
     });
     e.preventDefault();
   } else error = '';
